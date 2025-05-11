28aeac95ad83822c7f65228c947c612f34a1af5c
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2025-05-11 09:22:59 +0100

    Setup the project with Vite-TS

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-center mt-10 text-sky-500 text-2xl uppercase font-semibold">
+        Veriphye Coding Task
+      </h1>
+    </div>
+  );
+}
+
+export default App;
